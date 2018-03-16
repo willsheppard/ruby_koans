2b632ac46272603a42312e437cfa9de1a80373e3
Will Sheppard <nobody@example.com>
2018-03-16 11:14:39 +0000

    Initial solution

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,19 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+    attr_accessor :values
+
+    def roll(n)
+        @values = (1..n).to_a.map do |i|
+            _rand_num(6)
+        end
+    end
+
+    def _rand_num(p)
+        return 1 + rand(p)
+    end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -34,9 +44,11 @@ class AboutDiceProject < Neo::Koan
   def test_dice_values_should_change_between_rolls
     dice = DiceSet.new
 
+    srand(1)
     dice.roll(5)
     first_time = dice.values
 
+    srand(2)
     dice.roll(5)
     second_time = dice.values
 
@@ -48,6 +60,14 @@ class AboutDiceProject < Neo::Koan
     # If the rolls are random, then it is possible (although not
     # likely) that two consecutive rolls are equal.  What would be a
     # better way to test this?
+    #
+    # Answers:
+    # a) Mock the function that returns a random number.
+    #    Ensure the code is factored flexibly enough to easily allow this.
+    # b) Use srand.
+    # 
+    # See interesting discussion:
+    # https://stackoverflow.com/questions/2082970/whats-the-best-way-to-test-this
   end
 
   def test_you_can_roll_different_numbers_of_dice
