3a77a37b0b18274f402b0206711c2acd4f7845e4
Will Sheppard <nobody@example.com>
2018-04-18 12:44:56 +0100

    Finished to_str

--- koans/about_to_str.rb
+++ koans/about_to_str.rb
@@ -2,19 +2,33 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutToStr < Neo::Koan
 
+  # Explanation of the difference between .to_s and .to_str
+  #
+  # * "to_s" is a built-in that always returns some kind of string
+  #   representation of an object, even if it's not "useful",
+  #   e.g. looks like #<Demo:0x007fc49b05a408>
+  #
+  # * "to_str" is a user-defined method employed to actually state
+  #   that the object behaves like a string, and return a useful value.
+  #
+  # source: http://marcgg.com/blog/2017/01/23/ruby-to-s-to-str/
+
   class CanNotBeTreatedAsString
     def to_s
       "non-string-like"
     end
+    # This would make the assert_raise() fail
+    # as no exceptioni would be thrown
+    #alias :to_str :to_s
   end
 
   def test_to_s_returns_a_string_representation
     not_like_a_string = CanNotBeTreatedAsString.new
-    assert_equal __, not_like_a_string.to_s
+    assert_equal "non-string-like", not_like_a_string.to_s
   end
 
   def test_normally_objects_cannot_be_used_where_strings_are_expected
-    assert_raise(___) do
+    assert_raise(TypeError) do
       File.exist?(CanNotBeTreatedAsString.new)
     end
   end
@@ -33,11 +47,11 @@ class AboutToStr < Neo::Koan
 
   def test_to_str_also_returns_a_string_representation
     like_a_string = CanBeTreatedAsString.new
-    assert_equal __, like_a_string.to_str
+    assert_equal "string-like", like_a_string.to_str
   end
 
   def test_to_str_allows_objects_to_be_treated_as_strings
-    assert_equal __, File.exist?(CanBeTreatedAsString.new)
+    assert_equal false, File.exist?(CanBeTreatedAsString.new)
   end
 
   # ------------------------------------------------------------------
@@ -48,7 +62,7 @@ class AboutToStr < Neo::Koan
   end
 
   def test_user_defined_code_can_check_for_to_str
-    assert_equal __, acts_like_a_string?(CanNotBeTreatedAsString.new)
-    assert_equal __,  acts_like_a_string?(CanBeTreatedAsString.new)
+    assert_equal false, acts_like_a_string?(CanNotBeTreatedAsString.new)
+    assert_equal true,  acts_like_a_string?(CanBeTreatedAsString.new)
   end
 end
